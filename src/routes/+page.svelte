14c3d1d8eac452b68ded86bf263301e70905f3b0
TarzaxDev <98848694+TarzaxDev@users.noreply.github.com>
2024-08-29 01:35:06 +0200

    custom theme + start ✨

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center justify-center h-screen">
+    <h1 class="text-9xl font-bold">ParthenayEat</h1>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{html,svelte,js,ts}'],
+
     theme: {
         extend: {},
     },
+
     plugins: [require('daisyui')],
+
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    "primary": "#af1d2e",
+                    "secondary": "#af081c",
+                    "accent": "#f97282",
+                    "neutral": "#992c3a",
+                    "base-100": "#b23a48",
+                    "info": "#ffbfc6",
+                    "success": "#db2b40",
+                    "warning": "#890010",
+                    "error": "#ff0000",
+                },
+            },
+        ],
+    }
 }
